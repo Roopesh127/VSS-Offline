e0d2824e1d9571dc2e7e2799f392ee417d3632a3
Roopesh127 <roopesh.vishwakarma777@gmail.com>
2024-02-02 22:39:23 +0530

    02/02/2023

--- Destructuring/Array destructuring.js
+++ Destructuring/Array destructuring.js
@@ -1,9 +1,92 @@
 // Array destructuring 
 
-let arr = ["Roopesh","Sachin","Vishal","Pankaj"];
+// ---> 01
+
+// let arr = ["Roopesh","Sachin","Vishal","Pankaj"];
 
 // let a = arr[1];
 // console.log(a)
 
-const[a,b] = arr;
+// const[a,b] = arr;
 // console.log(a)
+
+// ---> 02
+
+// let arr = ["aalu","pyaj","bhata","bhata","tamatar","lehsun","adrak","dhniya","bhaji"];
+// let [one,two,three,four,five,six,seven,eight,nine] = arr;
+// console.log(one)
+// console.log(two)
+// console.log(nine)
+
+// ---> 03
+
+// let arr = ["aalu","pyaj","bhata","bhata","tamatar","lehsun","adrak","dhniya","bhaji"];
+
+// let[one , ...rest] = arr;
+// console
+
+
+
+
+// Q1>
+
+// getColor() 
+// function getColor(){ 
+// console.log("blue")
+//  }
+
+// console.log(x) 
+// var x =4
+
+// Output
+// ”Blue”
+// 4
+
+
+// Q2>
+// {
+//    Var x = 40
+//    Let y = 50
+// }
+
+// console.log(x)
+// console.log(y)
+
+
+// Output :- 
+// 40
+// Reference error
+
+
+// Q3>
+a()
+function a(){
+   var x = 40
+   let y = 50
+}
+
+console.log(x)
+console.log(y)
+
+
+// Output
+// 40
+// 50
+
+
+
+// q4>
+
+const d = () =>{
+   var x = 40
+   let y = 50
+}
+
+console.log(x)
+console.log(y)
+
+
+// Output
+// 40
+// Reference error
+
---
+++ Destructuring/Object destructuring.js
@@ -0,0 +1,68 @@
+// Object destructuring is the easy way to extract data from any object into a variable.
+// syntax ---> let {var1,var2} = {var1:...,var2:...}
+
+// Approch - 1
+
+// let obj = {
+//     fname : "roopesh",
+//     lname : "vishwakarma",
+//     school : "sdm"
+// }
+// let {fname,lname,school} = obj;
+// console.log(fname)
+// console.log(lname)
+// console.log(school)
+
+// Approach - 2
+
+// let obj = {
+//     fname : "sachin",
+//     lname : "Shrivastava",
+//     college : "srit",
+//     city : "jabalpur"
+// }
+
+// let {fname : pehlaname ,lname : akhriname , college : mahavidhyalaya , city : sahar } = obj;
+// console.log(pehlaname)
+// console.log(akhriname)
+
+// Approah --> 3 {by using of rest operator}
+
+// let obj = {
+//     fname : "sachin",
+//     lname : "Shrivastava",
+//     college : "srit",
+//     city : "jabalpur"
+// }
+
+// let {fname,...rest} = obj;
+// console.log(fname)
+// console.log(rest)
+
+// Approach --> 4 {for using of swapping value}
+
+// let obj = {
+//     fname : "sachin",
+//     lname : "Shrivastava",
+//     college : "srit",
+//     city : "jabalpur"
+// }
+// let{fname : lname , lname : fname} = obj;
+// console.log(fname)
+// console.log(lname)
+
+// Approach --> 5 {extracts values from properties to function parameter}
+
+// let obj = {
+//     fname : "sachin",
+//     lname : "Shrivastava",
+//     college : "srit",
+//     city : "jabalpur"
+// }
+
+// function getUserInfo({fname,lname}){
+//     console.log(`fullname =  ${fname} + ${lname} `)
+// }
+// getUserInfo(obj)
+
+// 
