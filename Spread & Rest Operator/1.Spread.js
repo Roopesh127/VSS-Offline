debdfa9be41314003b467c8cdbf9b0d246439f97
Roopesh127 <roopesh.vishwakarma777@gmail.com>
2024-02-05 22:37:54 +0530

    05/02

---
+++ Spread & Rest Operator/1.Spread.js
@@ -0,0 +1,23 @@
+// ---> 1. using for making copy.
+
+// let num = [1,2,3,4,5]
+// console.log(...num)
+
+// ---> 2. using for combine array
+
+// let arr1 = [1,3,5,7,9]
+// let arr2 = [2,4,6,8,10]
+
+// console.log(...arr1, ...arr2)
+
+// ---> 3. spread syntax to turn the string into array.
+
+// let str = "hello";
+// console.log([...str])
+
+// ---> 4. for making object
+
+// let obj = "hello";
+// console.log({...obj})
+
+// ---> 
