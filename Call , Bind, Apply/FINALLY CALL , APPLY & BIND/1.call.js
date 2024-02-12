53c64bacfcceda97de2c6d01641a472d52ea7902
Roopesh127 <roopesh.vishwakarma777@gmail.com>
2024-02-12 21:49:39 +0530

    12/02

---
+++ Call , Bind, Apply/FINALLY CALL , APPLY & BIND/1.call.js
@@ -0,0 +1,23 @@
+// # call , bind & apply all are function borrowing technique in javascript.
+// if we want to use one fuction method or variable in other then we are using it.
+
+
+
+// let obj = {
+//     fname : "Roopesh",
+//     lastname : "Vishwakarma",
+// }
+// function fullname (){
+//     return this.fname + this.lastname
+// }
+// console.log(fullname.call(obj))
+
+
+
+    // function greeting(greet){
+    //     return greet + " " + this.name1;
+    // }
+    // let user = {
+    //     name1 : "Sachin",
+    // }
+    // console.log(greeting.call(user,"Hello"));
