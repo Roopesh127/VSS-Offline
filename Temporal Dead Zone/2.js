4521c74956ca901efd0444e4ba844676db625ddf
Roopesh127 <roopesh.vishwakarma777@gmail.com>
2024-02-28 16:11:59 +0530

    28/02

---
+++ Temporal Dead Zone/2.js
@@ -0,0 +1,10 @@
+// ## Temporal Dead Zone => Temporal dead zone is a time since  when let and const variable are 
+//                          hoisted , and till it's initialized some value. Then time betweeen 
+//                          that known as Temporal Dead Zone.
+
+
+//                        ->  console.log(a); -> there a is hoisted and assign memory . It's through refrence error because of TDZ .
+
+//                        -> {{{ THESE TIME IS A TEMPORAL DEAD ZONE }}}
+
+//                        -> let a = 5;     -> assign value .
