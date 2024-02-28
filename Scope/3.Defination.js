4994defe83cbb43ac4255168295db25bf78896b3
Roopesh127 <roopesh.vishwakarma777@gmail.com>
2024-02-28 10:49:18 +0530

    28/02

--- Scope/3.Defination.js
+++ Scope/3.Defination.js
@@ -1,15 +1,30 @@
-// ## The range of function or varible where we can access it .
+// ## Scope =>  The range of function or varible where we can access it .
 
-var x= 5;
-function a(){
-   var y = 10;
-  function b(){
-    var z = 15;
+// var x= 5;
+// function a(){
+//    var y = 10;
+//   function b(){
+//     var z = 15;
  
-   console.log(x);
-   console.log(y);
-   console.log(z);
-}
-b();
-}
-a();
+//    console.log(x);
+//    console.log(y);
+//    console.log(z);
+// }
+// b();
+// }
+// a();
+
+
+
+// ## Scope Chain => Scope chain mean that if there is any variable is not found inhis
+//    own scope , then it's searching it's global scope if it's not inside i it's global then go to window 
+//    this hierarchy called scope chaining.
+
+   var x = 10;
+   function parent(){
+      function child(){
+         console.log(x);
+      }
+      child();
+   }
+   parent();
