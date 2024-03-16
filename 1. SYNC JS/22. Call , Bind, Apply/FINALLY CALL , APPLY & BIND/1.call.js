55b6ac33d2a45d135c9b5f83a8cba19a995ae36a
Roopesh127 <roopesh.vishwakarma777@gmail.com>
2024-03-16 08:51:42 +0530

    16/03

--- 1. SYNC JS/22. Call , Bind, Apply/FINALLY CALL , APPLY & BIND/1.call.js
+++ 1. SYNC JS/22. Call , Bind, Apply/FINALLY CALL , APPLY & BIND/1.call.js
@@ -21,3 +21,48 @@
     //     name1 : "Sachin",
     // }
     // console.log(greeting.call(user,"Hello"));
+
+
+// ----------------------------------------------------------------------------------------------------------------------------------------------------------
+// ----------------------------------------------------------------------------------------------------------------------------------------------------------
+
+// call , bind & Apply by Roadsidecoder
+
+
+
+// for without parameter
+
+// var obj = {
+//     name1 : "piyush"
+// }
+// function sayHello(){
+//     return "Hello" +" " + this.name1;
+// }
+// console.log(sayHello.call(obj))
+
+
+
+
+// for giving extra argument
+
+// let obj ={
+//     name1 : "piyush",
+// }
+// function sayHello(age){
+//     return "hello" + " " + this.name1 + " " + "is" + " " + age;
+// }
+// console.log(sayHello.call(obj,24));
+
+
+// for multiple argument 
+
+// let obj = {
+//     name1 : "Piyush",
+// }
+// function sayHello(age,city){
+//     return "Hello" + " " + this.name1 + " " + "is" + " "+ age + " from " + " " + city;
+// }
+// console.log(sayHello.call(obj ,22,"jabalpur"));
+
+
+
