33dc2c0a8d4071e4b2c6d64a62241404c680e0ea
Roopesh127 <roopesh.vishwakarma777@gmail.com>
2024-04-09 21:38:22 +0530

    09/04

--- 1. SYNC JS/14. map , resduce & filter/1. All.js
+++ 1. SYNC JS/14. map , resduce & filter/1. All.js
@@ -1,4 +1,4 @@
-// map method 
+// map method
 
 // --> iterated over aray , applied on array one by one , return always new array
 
@@ -7,15 +7,14 @@
 // console.log(x)
 // console.log(arr)
 
-
-// filter method
+// filter method : - filter method is always return a new array , it's iterate with all element in array but return only satisfied value only.
 
 // let arr = [1,2,3,4,5,6,7]
 // let x = arr.filter((x)=>x>3)
 // console.log(x)
 
-// reduce method 
+// reduce method :- We we have required to colled one value of multi value array then we are using reduce method and it's always rotate on each element of an array and return one value. It's take one accumulator value , if we are not provide accumulator then it's by default use first value as a accumulator.
 
-let arr = [1,2,3,4,5,6]
-let x = arr.reduce((acc,item)=>(acc + item)/item)
-console.log(x)
+let arr = [1, 2, 3, 4, 5, 6];
+let x = arr.reduce((acc, item) => (acc + item) / item);
+console.log(x);
