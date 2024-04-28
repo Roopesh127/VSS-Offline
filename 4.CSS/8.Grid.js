abce6c9240dcaa26bdcec2d6b87dde4205987f57
Roopesh127 <roopesh.vishwakarma777@gmail.com>
2024-04-28 12:18:36 +0530

    28/04

---
+++ 4.CSS/8.Grid.js
@@ -0,0 +1,3 @@
+// When we have required to control cotent in two direction then , there we are usig grid.
+// grid is a full page layout and flex box is for smaller content.
+//
---
+++ 4.CSS/9.postions.js
@@ -0,0 +1,14 @@
+// Positions stands for situation of any element there it is situated now.
+// -> there are 5 types of positioning properties in css.
+//
+//
+// 1- static => static is a default position of every element. top , bottom , right , left are not effect for this property.
+
+// 2- relative => it's pretty much same as static but it's affected by the properties of top,bottom,left & right.
+
+// 3- sticky => Moving or scrolling elements but fix in his own place. example :- inside linkedin upper part , chat board etc.
+
+// 4- absolute => this property use on the element which we want to position something at an exact point of screen , without disturbing any element.
+//     use cases:- modals , image with captions on it , icons on top of other element.
+
+// 5- fixed =>permanent fix in perticular place. used in navigation bar and chat. it's have top, bottom , right ,left prperties.
