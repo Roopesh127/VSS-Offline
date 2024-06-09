a8db944e61b035ae62f73e30317fa02eda284a5c
Roopesh127 <roopesh.vishwakarma777@gmail.com>
2024-06-09 15:08:04 +0530

    react fiber

---
+++ 5. React Interview questions/8. useReducer.js
@@ -0,0 +1,7 @@
+// Reducer are pure function that take a previous state and an action to return new state.
+
+// Note :- Reducer are pure functions so we should not mutate the state.
+
+// When To Use Reducer :- We can use reducer when our componet become too big , hard to read & debug due to it's state logic . There we can use reducer.
+
+
---
+++ 5. React Interview questions/9.React fiber.js
@@ -0,0 +1,5 @@
+// React fiber is a reconcilliation search engine of react which is introduced in React 16.
+
+// fiber mean that unit of work.
+
+// fiber in a react is just a plain javascript object  with some properties.
